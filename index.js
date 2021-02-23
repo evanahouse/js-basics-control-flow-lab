b9e91ddf1e6bb3b5f61c45c226905e8e221ca9dd
Evan <evanahouse@gmail.com>
2021-02-22 17:28:36 -0800

    Completed Assignment

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distance){
+  if (distance <= 400) {
+    return 'This one is on me!';
+  } else if (distance > 2500) {
+    return 'No can do.';
+  } 
+  else if (distance > 2000) {
+    return 'I will gladly take your thirty bucks.';
+  } 
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+
+function ternaryCheckCity(city){
+  let cityMessage;
+  city === 'NYC' ? (cityMessage = "Ok, sounds good.") : (cityMessage = "No go.");
+  return cityMessage;
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  let tipMessage;
+  switch (tip) {
+    case 'generous' :
+      return tipMessage = "Thank you so much.";
+    
+    case 'not as generous' :
+      return tipMessage = "Thank you.";
+
+    case 'thanks for everything' :
+      return tipMessage = "Bye.";
+
+  }
+
 }
